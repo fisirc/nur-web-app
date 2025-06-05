ecf010367ab144c49af1b513b4ceab2846018e8c
feelware <stevanpersonal@gmail.com>
2025-06-05 16:15:42 -0500

    fix: make active menu-item color lighter

--- dashboard/src/components/menuitem.tsx
+++ dashboard/src/components/menuitem.tsx
@@ -15,7 +15,7 @@ function MenuItem({
       <div
         className={`focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive inline-flex h-9 w-full shrink-0 items-center justify-start gap-2 rounded-md px-4 py-2 text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 has-[>svg]:px-3 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 ${
           active
-            ? "bg-primary/25"
+            ? "bg-primary/20"
             : "hover:bg-primary/5 hover:text-accent-foreground"
         } ${className} `}
       >

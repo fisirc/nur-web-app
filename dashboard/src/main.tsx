7861cd06b5b08e2c86f6ea93607853a2ac121e30
feelware <stevanpersonal@gmail.com>
2025-06-21 18:54:57 -0500

    fix: stop tanstack query from refetching on every dashboard tab change

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -5,7 +5,13 @@ import App from "./App.tsx";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 10,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

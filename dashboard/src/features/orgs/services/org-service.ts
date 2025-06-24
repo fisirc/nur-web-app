198c2f6c6925be601904d91ce59751887f42ad43
feelware <stevanpersonal@gmail.com>
2025-06-24 11:56:02 -0500

    refactor(org service): remove return type assertions

--- dashboard/src/features/orgs/services/org-service.ts
+++ dashboard/src/features/orgs/services/org-service.ts
@@ -2,6 +2,7 @@ import supabase from "@/services/supabase";
 
 import type { Member, Organization } from "../types";
 import type { Project } from "@/features/projects/types";
+import type { Func } from "@/features/functions/types";
 
 export default class OrgService {
   static getOrganization = async (org_id: string): Promise<Organization> => {
@@ -24,7 +25,7 @@ export default class OrgService {
     return data.map((item) => ({
       ...item.users,
       role: item.role,
-    })) as Member[];
+    }));
   };
 
   static getProjects = async (org_id: string): Promise<Project[]> => {
@@ -33,6 +34,6 @@ export default class OrgService {
       .select()
       .eq("organization_id", org_id);
     if (error) throw error;
-    return data as Project[];
+    return data;
   };
 }

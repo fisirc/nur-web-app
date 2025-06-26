d17dca62e78d15c83296b628a634469a7d9212f8
Stevan <stevanpersonal@gmail.com>
2025-06-26 14:34:46 -0500

    fix(table): disable color change on row hover

--- dashboard/src/components/ui/table.tsx
+++ dashboard/src/components/ui/table.tsx
@@ -55,7 +55,8 @@ function TableRow({ className, ...props }: React.ComponentProps<"tr">) {
     <tr
       data-slot="table-row"
       className={cn(
-        "data-[state=selected]:bg-muted hover:not-[thead_&]:bg-muted/50 border-b transition-colors",
+        "data-[state=selected]:bg-muted border-b transition-colors",
+        // "hover:not-[thead_&]:bg-muted/50",
         className,
       )}
       {...props}
